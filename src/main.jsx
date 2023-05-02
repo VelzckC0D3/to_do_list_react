0d1f35b7a42f0678617b37f8fb8121171a2be655
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-05-02 16:02:08 -0500

    components
    - added basic components with hardcoded information to render

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import TodoApp from '@/components/TodoApp';
+
+const domContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(domContainer);
+root.render(
+    <React.StrictMode>
+      <TodoApp />
+    </React.StrictMode>
+  );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
-})
+});
