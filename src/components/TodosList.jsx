c6523b0e2b0ceb40a7c5f03436f160363fbc2739
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-05-02 17:19:00 -0500

    checkbox interaction
    - added interaction to change the complete state with the checkbox for each item on the list

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,11 +1,15 @@
 import TodoItem from "@/components/TodoItem";
-const TodosList = ({ todosProps }) => {
+const TodosList = ({ todosProps, handleChange }) => {
   return (
     <ul>
-      {todosProps.map((todo) => (
-        <TodoItem key={todo.id} itemProp={todo} />
-      ))}
-    </ul>
+    {todosProps.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        itemProp={todo}
+        handleChange={handleChange}
+      />
+    ))}
+  </ul>
   );
 };
 export default TodosList;
--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,7 +1,9 @@
 import InputTodo from '@/components/InputTodo';
 import TodosList from '@/components/TodosList';
+import { useState } from 'react';
+
 const TodosLogic = () => {
-  const todos = [
+  const [todos, setTodos] = useState([
     {
       id: 1,
       title: 'Setup development environment',
@@ -17,11 +19,24 @@ const TodosLogic = () => {
       title: 'Deploy to live server',
       completed: false,
     },
-  ];
+  ]);
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+    prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    })
+  );
+};
   return (
     <div>
       <InputTodo />
-      <TodosList todosProps={todos} />
+      <TodosList todosProps={todos} handleChange={handleChange} />
     </div>
   );
 };
